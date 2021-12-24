014b33e0fb56feeac38353d75b1f863546d08858
xcwu <949696806@qq.com>
2021-12-24 11:58:42 +0800

    更新文件 GraphAnalysis.py

--- GraphAnalysis.py
+++ GraphAnalysis.py
@@ -1,3 +1,5 @@
+import random
+
 from CreateGraph import CreateGraph
 import json
 
@@ -10,24 +12,22 @@ from PageRank import PageRank
 
 
 class GraphAnalysis:
-    def __init__(self, G):
+    def __init__(self, G, DiG):
         self.G = G
+        self.DiG = DiG
         self.G.remove_edges_from(nx.selfloop_edges(G))
+        self.DiG.remove_edges_from(nx.selfloop_edges(DiG))
 
     def AnaDegree(self):
-        # print(self.G.in_degree())
         temp = []
-        with open("1.csv", "w", encoding='utf8') as f:
-            for x in nx.degree(self.G):
-                temp.append(x[1])
-                f.write(str(x[1]))
-                f.write('\n')
+        for x in nx.degree(self.G):
+            temp.append(x[1])
         temp.sort(reverse=True)
 
         plt.loglog(range(0, len(temp)), temp, 0.8)
-        plt.show()
+        plt.savefig("Degree_ll.jpg")
         plt.scatter(range(0, len(temp)), temp, 0.8)
-        plt.show()
+        plt.savefig("Degree_sc.jpg")
 
     def CN(self):
 
@@ -35,13 +35,27 @@ class GraphAnalysis:
         for u, v, p in preds:
             print(f"({u}, {v}) -> {p:.8f}")
 
+    def DevideDate(self):
+        count = 0
+        random.seed(1)
+        with open("test.txt", "w", encoding='utf8') as f:
+            while (count < 50000):
+                print(count)
+                i = random.randint(1, 944160 - 50000)
+                s, e = list(nx.edges(self.G))[i]
+                if nx.degree(self.G)[s] > 1 and nx.degree(self.G)[e] > 1:
+                    self.G.remove_edge(s, e)
+                    f.write(str(s) + ' ' + str(e))
+                    f.write('\n')
+                    count = count + 1
 
 
 if __name__ == '__main__':
     G = CreateGraph("课程设计数据集.txt").GetNx()
-    GA = GraphAnalysis(G)
-    GA.CN()
-
+    DiG = CreateGraph("课程设计数据集.txt").GetNxDi()
+    GA = GraphAnalysis(G, DiG)
+    a = GA.DevideDate()
+    pass
     # G.remove_edges_from(nx.selfloop_edges(G))
     # aaa = nx.core_number(G)
     # plt.scatter(range(0, len(aaa.values())), sorted(aaa.values()), 0.8)
